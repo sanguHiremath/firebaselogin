855cc565536924bc280bb707bacbfa2f104b2a26
Sangamesh Hiremath <sangamesh.hiremath@standardbank.co.za>
2023-04-25 12:19:03 +0530

    initial commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'firebaselogin' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Firebase'
+  pod 'Firebase/Auth'
+
+  target 'firebaseloginTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'firebaseloginUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ firebaselogin/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  firebaselogin
+//
+//  Created by Sangamesh on 25/04/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+        login(email: email.text ?? "", password: password.text ?? "")
+        
+    }
+    
+    func login(email:String, password:String){
+        Auth.auth().createUser(withEmail: (email), password: (password)) { (result, error) in
+                   if let _eror = error {
+                       //something bad happning
+                       print(_eror.localizedDescription )
+                   }else{
+                       //user registered successfully
+                       print(result ?? "")
+                   }
+                }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
